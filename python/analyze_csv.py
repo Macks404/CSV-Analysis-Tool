ad052d1353331c9590c99b7ee8429eb77dc2b46b
Max Jelley <jelleym@cardiff.ac.uk>
2026-05-26 19:09:14 +0100

    added repeated column deletion

--- python/analyze_csv.py
+++ python/analyze_csv.py
@@ -3,8 +3,39 @@
 import sys
 import json
 import pandas as pd
+import re
 
-def detect_column_types(df):
+def read_csv(file_path: str) -> tuple: # (DATAFRAME, ENCODING TYPE)
+    encodings = ["utf-8", "utf-8-sig", "cp1252", "latin1"]
+    error = None
+    for encoding in encodings:
+        try:
+            return pd.read_csv(file_path, encoding=encoding), encoding
+        except UnicodeDecodeError as err:
+            error = err
+    raise ValueError(f"Couldn't read CSV file due to encoding error: {error}")
+
+#### CLEANING FUNCTIONS ####
+def delete_repeated_columns(df: pd.DataFrame) -> pd.DataFrame:
+    repeated_columns = []
+    seen_base_names = set()
+
+    for column in df.columns:
+        # pandas automatically renames repeating column names to include a .1 or .2 etc..
+        base_name = re.sub(r"\.\d+$", "", column)
+
+        if base_name in seen_base_names:
+            repeated_columns.append(column)
+        else:
+            seen_base_names.add(base_name)
+    return df.drop(columns=repeated_columns)
+
+def clean_csv(df: pd.DataFrame) -> pd.DataFrame:
+    df = delete_repeated_columns(df)
+    return df
+
+#### ANALYSIS FUNCTIONS ####
+def detect_column_types(df) -> dict:
     results = {}
 
     for col in df.columns:
@@ -33,19 +64,9 @@ def detect_column_types(df):
 
     return results
 
-def read_csv(file_path: str):
-    encodings = ["utf-8", "utf-8-sig", "cp1252", "latin1"]
-    error = None
-    for encoding in encodings:
-        try:
-            return pd.read_csv(file_path, encoding=encoding), encoding
-        except UnicodeDecodeError as err:
-            error = err
-    raise ValueError(f"Couldn't read CSV file due to encoding error: {error}")
-
-
-def analyze_csv(file_path: str):
+def analyze_csv(file_path: str) -> dict:
     df, enc = read_csv(file_path)
+    df = clean_csv(df)
     column_types = detect_column_types(df)
 
     result = {
@@ -56,7 +77,7 @@ def analyze_csv(file_path: str):
         "dataTypes": df.dtypes.apply(lambda x: x.name).to_dict(),
         "missingValues": df.isnull().sum().to_dict(),
         "uniqueValues": {col: df[col].nunique() for col in df.columns},
-        "columnTypes": column_types
+        "columnTypes": column_types,
     }
     
     return result
